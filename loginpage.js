7287663054c2f5cf0486eaf138a371645004e822
unknown <Anup@LAPTOP-4L0F33J3>
2025-02-03 01:23:31 +0530

    first commit

---
+++ loginpage.js
@@ -0,0 +1,7 @@
+let password = document.getElementById("password"); 
+let email = document.getElementById("email"); 
+
+function validate(){ 
+    document.getElementById("message").textContent = "Hi " + email.value+" we are validating your password"; ;
+      
+}    
